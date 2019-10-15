f5b32db9a9be669768b6c0d11517e812d9a05183
Steve Winton <stevewinton@gmail.com>
2019-10-14 20:23:36 -0500

    Remove program, usage

--- action.js
+++ action.js
@@ -1,24 +1,15 @@
-#!/usr/bin/env node
-// Usage: probot receive -e push -p path/to/payload app.js
-
 process.env.DISABLE_STATS = 'true'
 
 const path = require('path')
 const uuid = require('uuid')
-const program = require('commander')
 
 const core = require('@actions/core');
 
 const { createProbot } = require('probot')
 
-program
-  .usage('[options] [path/to/app.js...]')
-  .parse(process.argv)
-
 const githubToken = process.env.GITHUB_TOKEN
 const event = process.env.GITHUB_EVENT_NAME
 const payloadPath = process.env.GITHUB_EVENT_PATH
-
 const payload = require(path.resolve(payloadPath))
 
 const probot = createProbot({
@@ -27,9 +18,7 @@ const probot = createProbot({
 })
 
 // TODO: allow path to handler to be specified externally
-const args = program.args.length && program.args || [ './index.js' ]
-core.debug(`Setting up Probot with args ${ JSON.stringify(args) }`)
-probot.setup(args)
+probot.setup([ './index.js' ])
 
 core.debug(`Receiving event ${ JSON.stringify(event) }`)
 probot.receive({ name: event, payload, id: uuid.v4() }).catch((err) => {

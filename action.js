6d1f73d589bc9bd77cea896fab91f6f8f73e740c
Steve Winton <stevewinton@gmail.com>
2019-10-14 20:21:39 -0500

    Remove --event, --payload-path options

--- action.js
+++ action.js
@@ -13,17 +13,13 @@ const { createProbot } = require('probot')
 
 program
   .usage('[options] [path/to/app.js...]')
-  .option('-e, --event <event-name>', 'Event name', process.env.GITHUB_EVENT_NAME)
-  .option('-p, --payload-path <payload-path>', 'Path to the event payload', process.env.GITHUB_EVENT_PATH)
   .parse(process.argv)
 
 const githubToken = process.env.GITHUB_TOKEN
+const event = process.env.GITHUB_EVENT_NAME
+const payloadPath = process.env.GITHUB_EVENT_PATH
 
-if (!program.event || !program.payloadPath) {
-  program.help()
-}
-
-const payload = require(path.resolve(program.payloadPath))
+const payload = require(path.resolve(payloadPath))
 
 const probot = createProbot({
   cert: null,
@@ -35,8 +31,8 @@ const args = program.args.length && program.args || [ './index.js' ]
 core.debug(`Setting up Probot with args ${ JSON.stringify(args) }`)
 probot.setup(args)
 
-core.debug(`Receiving event ${ JSON.stringify(program.event) }`)
-probot.receive({ name: program.event, payload, id: uuid.v4() }).catch((err) => {
+core.debug(`Receiving event ${ JSON.stringify(event) }`)
+probot.receive({ name: event, payload, id: uuid.v4() }).catch((err) => {
   // setFailed logs the message and sets a failing exit code
   core.setFailed(`Action failed with error ${err}`);
 })

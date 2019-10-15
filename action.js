c80070e50d3d75045de50f0bc229eceef6c049b4
Steve Winton <stevewinton@gmail.com>
2019-10-15 07:12:09 -0500

    Use adapter for 'heavy lifting'

--- action.js
+++ action.js
@@ -1,24 +1,5 @@
-process.env.DISABLE_STATS = 'true'
+const probotActionsAdapter = require('./lib/probot-actions-adapter')
 
-const path = require('path')
-const uuid = require('uuid')
-
-const core = require('@actions/core')
-
-const { createProbot } = require('probot')
 const handler = require('./index')
 
-// Setup Probot app
-const githubToken = process.env.GITHUB_TOKEN
-const probot = createProbot({ githubToken })
-probot.setup([ handler ])
-
-// Process the event
-const event = process.env.GITHUB_EVENT_NAME
-const payloadPath = process.env.GITHUB_EVENT_PATH
-const payload = require(path.resolve(payloadPath))
-core.debug(`Receiving event ${ JSON.stringify(event) }`)
-probot.receive({ name: event, payload, id: uuid.v4() }).catch((err) => {
-  // setFailed logs the message and sets a failing exit code
-  core.setFailed(`Action failed with error ${err}`)
-})
+probotActionsAdapter(handler)

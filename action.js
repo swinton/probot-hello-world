561701a40e01b3e3e9d78280c1fee142e9f7745e
Steve Winton <stevewinton@gmail.com>
2019-10-14 20:33:56 -0500

    Setup with ApplicationFunction

--- action.js
+++ action.js
@@ -3,9 +3,10 @@ process.env.DISABLE_STATS = 'true'
 const path = require('path')
 const uuid = require('uuid')
 
-const core = require('@actions/core');
+const core = require('@actions/core')
 
 const { createProbot } = require('probot')
+const handler = require('./index')
 
 const githubToken = process.env.GITHUB_TOKEN
 const event = process.env.GITHUB_EVENT_NAME
@@ -17,8 +18,7 @@ const probot = createProbot({
   githubToken
 })
 
-// TODO: allow path to handler to be specified externally
-probot.setup([ './index.js' ])
+probot.setup([ handler ])
 
 core.debug(`Receiving event ${ JSON.stringify(event) }`)
 probot.receive({ name: event, payload, id: uuid.v4() }).catch((err) => {
